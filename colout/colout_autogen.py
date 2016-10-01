6a6fa5732a673b0cc6cd08d5be1d874512850875
Omer Katz <omer.drow@gmail.com>
2016-10-01 16:46:55 +0300

    Added a theme for autogen.

---
+++ colout/colout_autogen.py
@@ -0,0 +1,16 @@
+from colout import colout_configure
+from colout.colout import ThemeEntry
+
+
+def theme(context):
+    context, th = colout_configure.theme(context)
+    th.extend([
+        ThemeEntry("error: ", "red", "bold"),
+        ThemeEntry("warning: ", "magenta", "bold"),
+        ThemeEntry(r"^[^\s:]*:[0-9:]*", "blue", "bold"),
+        ThemeEntry(r"AC_[A-Z_]+", "cyan"),
+        ThemeEntry(r"ACLOCAL_[A-Z_]+", "cyan"),
+        ThemeEntry(r"'.*'[\s|.]", "yellow"),
+        ThemeEntry(r"\".*\"\s", "yellow")
+    ])
+    return context, th
